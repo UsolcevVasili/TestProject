efb9aa36742eceb3e6b2a299f780013797384194
Vasili <Vasili@MacBook-Pro-Vasili.local>
2016-07-29 01:22:13 +0600

    Hello World

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Vasili on 19.07.16.
+//  Copyright © 2016 Vasili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Vasili on 19.07.16.
+//  Copyright © 2016 Vasili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
